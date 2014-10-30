3f29408d2b7142cd4f157b58573979db90ba83b0
victorarcasrios <victorarcasrios@gmail.com>
2014-10-30 09:23:24 +0100

    Logic implemented

--- LinearLayout/app/src/main/java/com/example/victorarcasrios/linearlayout/MainActivity.java
+++ LinearLayout/app/src/main/java/com/example/victorarcasrios/linearlayout/MainActivity.java
@@ -1,12 +1,28 @@
 package com.example.victorarcasrios.linearlayout;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Hashtable;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private HashMap colors = new HashMap<Integer, Integer>(){{
+        put(R.id.redRB, Color.RED);
+        put(R.id.whiteRB, Color.WHITE);
+        put(R.id.blueRB, Color.BLUE);
+    }};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +51,18 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setColor(View view){
+        TextView textView = (TextView)findViewById(R.id.textView);
+        RadioGroup radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
+        int id = radioGroup.getCheckedRadioButtonId();
+
+        textView.setBackgroundColor((Integer) colors.get(id));
+    }
+
+    public void clearTextView(View view){
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setBackgroundColor(Color.LTGRAY);
     }
 }
